c657af072911d296960cab2038d9f6681b2c6c07
pineakl <lukman.alfian@gmail.com>
2023-05-12 13:32:25 +0700

    Fix aspect ratio UI scaling

---
+++ Assets/Game05-Runner/script/PropReplace.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropReplace : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> _propSprites;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
